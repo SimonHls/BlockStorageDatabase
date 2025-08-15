2c89814ef707c4cc4edf07445e6dd10b46b305e5
Simon Hülskamp <s.huelskamp@ricon.de>
2025-08-15 14:31:45 +0200

    established project structure

---
+++ BlockStorageDatabase/src/BlockStorageCLI/Program.cs
@@ -0,0 +1,2 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World! Even though this is just the initial commit, i at least did something (writing this)");
---
+++ BlockStorageDatabase/src/BlockStorageDatabase/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace BlockStorageDatabase {
+    public class Class1 {
+
+    }
+}
