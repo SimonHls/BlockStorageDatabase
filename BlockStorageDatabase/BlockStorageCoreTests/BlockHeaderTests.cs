ff1db99e3db4ab5a0ba0024b18e6f8875ed11b22
Simon Hülskamp <s.huelskamp@ricon.de>
2025-08-20 16:01:56 +0200

    implemented block header set and get methods, with tests for basic use cases

---
+++ BlockStorageDatabase/BlockStorageCoreTests/BlockHeaderTests.cs
@@ -0,0 +1,74 @@
+using BlockStorageCore.Entities;
+
+namespace BlockStorageCoreTests;
+
+public class BlockHeaderTests : IDisposable {
+
+    private readonly MemoryStream _stream;
+    private readonly Block _block;
+    private readonly long[] _initialHeaderData = { 1, 2, 3, 4, 5, 6 };
+
+    public BlockHeaderTests() {
+        _stream = new MemoryStream();
+        using (var writer = new BinaryWriter(_stream, System.Text.Encoding.UTF8, leaveOpen: true)) {
+            foreach (var value in _initialHeaderData) {
+                writer.Write(value);
+            }
+        }
+        _stream.Position = 0;
+
+        _block = new Block(_stream, blockSize: 4096, blockId: 0);
+    }
+
+    public void Dispose() {
+        GC.SuppressFinalize(GetType());
+        _stream.Dispose();
+    }
+
+    [Fact]
+    public void GetHeader_ReturnsCorrectHeaderFromValidStream() {
+        // == Act ==
+        var headerVal = _block.GetHeader(1);
+
+        // == Assert ==
+        Assert.Equal(2, headerVal);
+    }
+
+    [Fact]
+    public void GetHeader_ThrowsObjectDisposedException_WhenCalledOnDisposedObject() {
+        // == Arrange ==
+        _block.Dispose();
+
+        // == Act ==
+        Action act = () => _block.GetHeader(1); ;
+        var ex = Record.Exception(act);
+
+        // == Assert ==
+        Assert.NotNull(ex);
+        Assert.IsType<ObjectDisposedException>(ex);
+    }
+
+    [Fact]
+    public void SetHeader_CanUpdateHeaderValueOnValidStream() {
+        // == Act ==
+        _block.SetHeader(1, 3); // change index 1 from 2 to 3
+        var headerVal = _block.GetHeader(1);
+
+        // == Assert ==
+        Assert.Equal(3, headerVal);
+    }
+
+    [Fact]
+    public void SetHeader_ThrowsObjectDisposedException_WhenCalledOnDisposedObject() {
+        // == Arrange ==
+        _block.Dispose();
+
+        // == Act ==
+        Action act = () => _block.SetHeader(1, 3);
+        var ex = Record.Exception(act);
+
+        // == Assert ==
+        Assert.NotNull(ex);
+        Assert.IsType<ObjectDisposedException>(ex);
+    }
+}
--- BlockStorageDatabase/src/BlockStorageCore/Entities/Block.cs
+++ BlockStorageDatabase/src/BlockStorageCore/Entities/Block.cs
@@ -9,48 +9,73 @@ public class Block : IBlock {
     private Stream _stream;
     private readonly int _blockSize;
 
-    // variable to track if the block was changed
+    private bool isDisposed = false;
+
+    // variable to track if the block was changed after the intial read
     private bool pendingChanges = false;
 
-    // The block data
-    public byte[] Header { get; set; }
-    public byte[] Data { get; set; }
+    // We store header data in a cache, so we can get it faster on second reads
+    public long?[] headerCache = new long?[BlockConstants.HeaderFieldCount];
 
     public Block(Stream stream, int blockSize, uint blockId) {
         _stream = stream;
         _blockSize = blockSize;
-        Data = new byte[blockSize];
-        Header = new byte[BlockConstants.HeaderSize];
         Id = blockId;
-    }
-
-    public void Dispose() {
-        if (pendingChanges)
-            // We dump the block into the stack
-            throw new NotImplementedException();
-        else
-            // we dispose the block
-            throw new NotImplementedException();
     }
 
     public long GetHeader(int field) {
         // Todo: Headers could also be an enum, this would be way more readable
-        var buffer = new byte[BlockConstants.HeaderFieldSize];
+
+        if (isDisposed)
+            throw new ObjectDisposedException(nameof(Block));
+
+        if (field < 0 || field > BlockConstants.HeaderFieldCount)
+            throw new ArgumentOutOfRangeException(nameof(field), "Could not get header, index out of range.");
+
         long positionInStream = _blockSize * Id + (long)field * BlockConstants.HeaderFieldSize;
-        _stream.Seek(positionInStream, 0);
+        if (positionInStream > _stream.Length || positionInStream < 0)
+            throw new ArgumentOutOfRangeException(nameof(field), "Could not get header, calculated header position is outside the stream,");
+
+        var buffer = new byte[BlockConstants.HeaderFieldSize];
+        _stream.Seek(positionInStream, (int)SeekOrigin.Begin);
         _stream.Read(buffer, 0, BlockConstants.HeaderFieldSize);
         return BufferHelper.ReadBufferInt64(buffer, 0);
+    }
+
+    public void SetHeader(int field, long value) {
+        if (isDisposed)
+            throw new ObjectDisposedException(nameof(Block));
+
+        if (field < 0 || field > BlockConstants.HeaderFieldCount)
+            throw new ArgumentOutOfRangeException(nameof(field), "Could not get header, index out of range.");
+
+        long positionInStream = _blockSize * Id + (long)field * BlockConstants.HeaderFieldSize;
+        if (positionInStream > _stream.Length || positionInStream < 0)
+            throw new ArgumentOutOfRangeException(nameof(field), "Could not get header, calculated header position is outside the stream,");
+
+        var valueBuffer = new byte[ByteLengths.LongLen];
+
+        BufferHelper.WriteBuffer(value, valueBuffer, (int)SeekOrigin.Begin);
+
+        _stream.Seek(positionInStream, 0);
+        _stream.Write(valueBuffer, 0, valueBuffer.Length);
     }
 
     public void Read(byte[] dst, int dstOffset, int srcOffset, int count) {
         throw new NotImplementedException();
     }
 
-    public void SetHeader(int field, long value) {
+    public void Write(byte[] src, int srcOffset, int dstOffset, int count) {
         throw new NotImplementedException();
     }
 
-    public void Write(byte[] src, int srcOffset, int dstOffset, int count) {
-        throw new NotImplementedException();
+    public void Dispose() {
+        if (pendingChanges) {
+            _stream.Flush();
+        }
+        if (!isDisposed) {
+            _stream.Dispose();
+            isDisposed = true;
+        }
     }
 }
